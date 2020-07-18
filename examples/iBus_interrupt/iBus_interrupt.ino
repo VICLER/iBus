6895e09c6754ede5016729f37dbd109167c650f1
Victor Glekler <victor.glekler@gmail.com>
2020-07-18 21:23:17 +0200

    upd

--- examples/iBus_interrupt/iBus_interrupt.ino
+++ examples/iBus_interrupt/iBus_interrupt.ino
@@ -1,18 +1,17 @@
 #include <iBus.h>
 
-#define CHANNELS 14
-iBus receiver(Serial, CHANNELS);
+#define MAX_CHANNELS 14
+iBus receiver(Serial, MAX_CHANNELS);
 
 void setupTimer(){
   cli();
   TCCR2A = 0;
   TCCR2B = 0;
   TCNT2  = 0;
-  OCR2A = 125;                              // 500us period
-  TCCR2A |= (1 << WGM21);                  // turn on CTC mode
-  //TCCR2B |= (1 << CS20) | (1 << CS21);  // 32 prescaler for 8Mhz Clock
-  TCCR2B |= (1 << CS22);                 // 64 prescaler for 16Mhz Clock
-  TIMSK2 |= (1 << OCIE2A);              // enable timer compare interrupt
+  TCCR2B |= (1 << CS22);       // 64 prescaler
+  OCR2A = 125;                // 500us period
+  TCCR2A |= (1 << WGM21);    // turn on CTC mode
+  TIMSK2 |= (1 << OCIE2A);  // enable timer compare interrupt
   sei();
 }
 
@@ -23,14 +22,16 @@ void setup() {
 }
 
 void loop() {
-  for(byte i = 1; i <= CHANNELS; i++){  // get channel values starting from 1
+
+  digitalWrite(LED_BUILTIN, receiver.available());  // turn led on if connected
+
+  for(byte i = 1; i <= MAX_CHANNELS; i++){  // get channel values starting from 1
     Serial.print(receiver.get(i));
     Serial.print('\t');
   }
   Serial.println(receiver.get(0));  //  indicates received packet quality. If 0 -> there are some corrupted values
   delay(10);
 
-  digitalWrite(LED_BUILTIN, receiver.available());
 }
 
 ISR(TIMER2_COMPA_vect)
--- examples/iBus_read/iBus_read.ino
+++ examples/iBus_read/iBus_read.ino
@@ -10,14 +10,16 @@ void setup() {
 }
 
 void loop() {
+
   receiver.process();
 
   digitalWrite(LED_BUILTIN, receiver.available());  // turn led on if connected
 
-  for(byte i = 1; i <= CHANNELS; i++){  // get channel values starting from 1
+  for(byte i = 1; i <= MAX_CHANNELS; i++){  // get channel values starting from 1
     Serial.print(receiver.get(i));
     Serial.print('\t');
   }
-  Serial.println(receiver.get(0));  //  indicates received packet quality. If 0 -> there are some corrupted values
+  Serial.println(receiver.get(0));  // received packet quality. If 0 -> there are some corrupted values
   delay(10);
+  
 }
